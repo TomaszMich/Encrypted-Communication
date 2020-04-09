21c4c99be871411951ba401d3eec6efba14a05c5
Tomasz Michalski <tomasz.michals@gmail.com>
2020-04-09 19:18:53 +0200

    Fix imports

--- src/core/data_receiver.py
+++ src/core/data_receiver.py
@@ -1,7 +1,7 @@
 import socketserver
 import threading
 
-from .constants import SERVER_ADDRESS, SERVER_PORT
+from constants import SERVER_ADDRESS, SERVER_PORT
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
@@ -28,9 +28,10 @@ class DataReceiver:
         self.server = Server(self.address, RequestHandler)
 
     def start_receiving(self):
-        rcv_thread = threading.Thread(target=self.server.serve_forever)
-        rcv_thread.setDaemon(True)
-        rcv_thread.start()
+        self.server.serve_forever()
+        # rcv_thread = threading.Thread(target=self.server.serve_forever)
+        # rcv_thread.setDaemon(True)
+        # rcv_thread.start()
 
     def stop_receiving(self):
         self.server.shutdown()
--- src/core/main.py
+++ src/core/main.py
@@ -1,7 +1,10 @@
 
-from .data_receiver import DataReceiver
-from .data_sender import DataSender
+from data_receiver import DataReceiver
+from data_sender import DataSender
 
 if __name__ == '__main__':
     receiver = DataReceiver()
     receiver.start_receiving()
+
+    # sender = DataSender("25.119.38.155", 8080)
+    # sender.send_data("Test message".encode())
