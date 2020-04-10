4616fe37ffd0d6cf9d459bb62f08e31728d7573a
Your Name <you@example.com>
2020-04-10 15:01:13 +0200

    Added GUI file

--- src/core/main.py
+++ src/core/main.py
@@ -3,8 +3,8 @@ from data_receiver import DataReceiver
 from data_sender import DataSender
 
 if __name__ == '__main__':
-    receiver = DataReceiver()
-    receiver.start_receiving()
+    sender = DataSender("25.119.38.155", 8080)
+    sender.send_data("Test message".encode())
 
     # sender = DataSender("25.119.38.155", 8080)
     # sender.send_data("Test message".encode())
