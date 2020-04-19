0d6ce296f17aaadc327edb918b4e50ff8897a24b
Tomasz Michalski <tomasz.michals@gmail.com>
2020-04-19 13:59:23 +0200

    Refactor GUI code

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,76 +1,76 @@
+import sys
+from PyQt5.QtWidgets import QFileDialog, QWidget, QApplication, QPushButton, QLabel, QGridLayout, QProgressBar, \
+    QRadioButton, QLineEdit
 
-import sys, time
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog, QWidget, QApplication, QPushButton, QLabel, QGridLayout, QProgressBar, QRadioButton, QMessageBox, QLineEdit
 
-class Gui(QWidget):
+class EntryApp(QWidget):
     def __init__(self):
         super().__init__()
-
-        self.init_ui()
-
-    def init_ui(self):
-        self.browseButton = QPushButton('Browse')
-        self.sendButton = QPushButton('Send')
-        self.progressBar = QProgressBar()
-        self.radioButton1 = QRadioButton('ECB')
-        self.radioButton2 = QRadioButton('CBC')
-        self.radioButton3 = QRadioButton('CFB')
-        self.radioButton4 = QRadioButton('OFB')
-        self.currentMessage = QLabel('Please select mode and file')
-
-        grid = QGridLayout()
-        grid.setSpacing(10)
-        grid.addWidget(self.browseButton, 4, 3)
-        grid.addWidget(self.sendButton, 4, 4)
-        grid.addWidget(self.progressBar, 0, 0, 1, 0)
-        grid.addWidget(self.radioButton1, 1, 0)
-        grid.addWidget(self.radioButton2, 2, 0)
-        grid.addWidget(self.radioButton3, 3, 0)
-        grid.addWidget(self.radioButton4, 4, 0)
-        grid.addWidget(self.currentMessage, 1, 1, 1, 3)
-
-        self.setLayout(grid)
-        self.setGeometry(700, 400, 400, 200)
-        self.setWindowTitle('Jakub Wlostowski & Tomasz Michalski')
-        self.show()
-        self.browseButton.clicked.connect(self.browseFiles)
-
-    def browseFiles(self):
-        filename = QFileDialog.getOpenFileName()
-        path = filename[0]
-        self.currentMessage.setText(path)
-        return path
-
-
-class pop_up(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.key = ' '
-        self.init_ui()
-
-    def init_ui(self):
-        grid = QGridLayout()
-        grid.setSpacing(10)
-
+        self.key = ''
+        self.layout = QGridLayout()
+        self.layout.setSpacing(10)
         self.textbox = QLineEdit()
-        self.label = QLabel('Please enter your encryption key below')
-        self.confirmButton = QPushButton('Confirm')
-        grid.addWidget(self.label, 0, 0)
-        grid.addWidget(self.textbox, 1, 0)
-        grid.addWidget(self.confirmButton, 1, 1)
-        self.setLayout(grid)
+        self.label = QLabel("Please provide an access key, which will be used to encrypt RSA key:")
+        self.confirm_button = QPushButton('Confirm')
+        self._add_widgets()
+
+        self.setLayout(self.layout)
         self.setGeometry(700, 400, 400, 100)
         self.setWindowTitle('Welcome!')
+        self.confirm_button.clicked.connect(self._confirm_click)
         self.show()
-        self.confirmButton.clicked.connect(self.confirm_click)
 
-    def confirm_click(self):
+    def _confirm_click(self):
         self.key = self.textbox.text()
         self.hide()
-        self.gui = Gui()
+        self.main_app = MainApp()
+
+    def _add_widgets(self):
+        self.layout.addWidget(self.label, 0, 0)
+        self.layout.addWidget(self.textbox, 1, 0)
+        self.layout.addWidget(self.confirm_button, 1, 1)
+
+
+class MainApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QGridLayout()
+        self.layout.setSpacing(10)
+        self.browse_button = QPushButton('Browse')
+        self.send_button = QPushButton('Send')
+        self.radio_button_ecb = QRadioButton('ECB')
+        self.radio_button_cbc = QRadioButton('CBC')
+        self.radio_button_cfb = QRadioButton('CFB')
+        self.radio_button_ofb = QRadioButton('OFB')
+        self.progress_bar = QProgressBar()
+        self.dynamic_message = QLabel('Please select mode and file')
+        self._add_widgets()
+
+        self.setLayout(self.layout)
+        self.setGeometry(700, 400, 400, 200)
+        self.setWindowTitle('Jakub Wlostowski & Tomasz Michalski')
+
+        self.browse_button.clicked.connect(self._browse_files)
+        self.show()
+
+    def _browse_files(self):
+        filename = QFileDialog.getOpenFileName()
+        path = filename[0]
+        self.dynamic_message.setText(f"Selected file:\n{path}")
+        return path
+
+    def _add_widgets(self):
+        self.layout.addWidget(self.browse_button, 4, 3)
+        self.layout.addWidget(self.send_button, 4, 4)
+        self.layout.addWidget(self.progress_bar, 0, 0, 1, 0)
+        self.layout.addWidget(self.radio_button_ecb, 1, 0)
+        self.layout.addWidget(self.radio_button_cbc, 2, 0)
+        self.layout.addWidget(self.radio_button_cfb, 3, 0)
+        self.layout.addWidget(self.radio_button_ofb, 4, 0)
+        self.layout.addWidget(self.dynamic_message, 1, 1, 1, 3)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    start = pop_up()
+    start = EntryApp()
     sys.exit(app.exec_())
