b85d5440931bb916a49581afcb1dae80156be4ac
Jakub Wlostowski <jakub.wlostowski@gmail.com.com>
2020-04-22 19:02:20 +0200

    Fixed sending messages

--- src/core/data_receiver.py
+++ src/core/data_receiver.py
@@ -1,10 +1,37 @@
 import socketserver
 import threading
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QGridLayout
+
+
+class ReceivedWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QGridLayout()
+        self.layout.setSpacing(10)
+        self.proceed_button = QPushButton("Ok")
+        self.message_received = QLabel("Placeholder")
+
+        self.setLayout(self.layout)
+        self.setGeometry(700, 400, 300, 100)
+        self.setWindowTitle('Received a file')
+        self._add_widgets()
+
+        self.proceed_button.clicked.connect(self._confirm_click)
+        self.show()
+
+    def _add_widgets(self):
+        self.layout.addWidget(self.proceed_button, 1, 1, 1, 1)
+        self.layout.addWidget(self.message_received, 0, 0)
+
+    def _confirm_click(self):
+        self.hide()
 
 
 class RequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         data = self.request.recv(4096)
+        # r = ReceivedWindow()
+        # r.show()
         print(data)
         self.request.send("Received data".encode())
         return
@@ -23,10 +50,10 @@ class DataReceiver:
         self.server = Server(self.address, RequestHandler)
 
     def start_receiving(self):
-        self.server.serve_forever()
-        # rcv_thread = threading.Thread(target=self.server.serve_forever)
-        # rcv_thread.setDaemon(True)
-        # rcv_thread.start()
+       # self.server.serve_forever()
+        rcv_thread = threading.Thread(target=self.server.serve_forever)
+        rcv_thread.setDaemon(True)
+        rcv_thread.start()
 
     def stop_receiving(self):
         self.server.shutdown()
--- src/core/data_sender.py
+++ src/core/data_sender.py
@@ -1,14 +1,18 @@
 import socket
 import logging
+import os
 from fsplit.filesplit import FileSplit
 
 log = logging.getLogger(__name__)
+
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 4096
 
 
 class DataSender:
     def __init__(self, config):
         self.address = (config.receiver_ip, config.connection_port)
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send_data(self, data):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -16,6 +20,22 @@ class DataSender:
             sock.connect(self.address)
             log.info(f"Sending {data}")
             sock.send(data)
+
+    def send_file(self, filename, progress_bar):
+        file_size = os.path.getsize(filename)
+        total_bytes_read = 0
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            log.info(f"Connecting to {self.address}")
+            s.connect(self.address)
+            s.send(f"{filename}{SEPARATOR}{file_size}".encode())
+            with open(filename, "rb") as file:
+                while True:
+                    bytes_read = file.read(BUFFER_SIZE)
+                    total_bytes_read += bytes_read
+                    if not bytes_read:
+                        break
+                    s.sendall(bytes_read)
+                    progress_bar.setValue((int(bytes_read) / total_bytes_read) * 100)
 
     def split_data(self, filename):
         fs = FileSplit(filename, 5)
